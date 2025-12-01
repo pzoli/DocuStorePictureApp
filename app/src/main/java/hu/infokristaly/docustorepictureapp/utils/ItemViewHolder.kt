2e9900fe296d7a988eb1918d1eca045f05110052
pzoli <papp.zoltan@integrity.hu>
2025-12-01 16:15:06 +0100

    added doc list item location

--- app/src/main/java/hu/infokristaly/docustorepictureapp/utils/ItemViewHolder.kt
+++ app/src/main/java/hu/infokristaly/docustorepictureapp/utils/ItemViewHolder.kt
@@ -11,10 +11,12 @@ class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val itemTextView: TextView = itemView.findViewById(R.id.item_text)
     private val itemDateView: TextView = itemView.findViewById(R.id.item_date)
     private val itemCommentView: TextView = itemView.findViewById(R.id.item_comment)
+    private val itemLocationView: TextView = itemView.findViewById(R.id.item_location)
     fun bind(docInfo: DocInfo) {
         val formatedCreatedAt = SimpleDateFormat("yyyy-MM-dd HH:mm").format(docInfo.createdAt!!)
         itemDateView.text = formatedCreatedAt
         itemTextView.text = "${docInfo.organization?.name} - ${docInfo.subject?.value}"
         itemCommentView.text = docInfo.comment
+        itemLocationView.text = docInfo.docLocation?.getLocatoinPath()
     }
 }
